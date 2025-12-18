af443eaea590f0817c5665448017a5f23ca9e7a7
SonTC <sontc@smac.com.vn>
2025-12-18 14:24:35 +0700

    sontc  - fix build android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.sontc.financeapp"
+    namespace = "com.sontc.financeappv1"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.sontc.financeapp"
+        applicationId = "com.sontc.financeappv1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/java/com/sontc/financeappv1/MainActivity.java
+++ android/app/src/main/java/com/sontc/financeappv1/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.flutter_money_management;
+package com.sontc.financeappv1;
 
 import io.flutter.embedding.android.FlutterActivity;
 
